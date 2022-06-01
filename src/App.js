91c2d340a127eee3499c296005a01aac95bb7e17
Constant Druon <druon.constant@gmail.com>
2022-06-01 17:29:37 +0200

    try

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import { useTranslation } from 'react-i18next';
-import logo from './logo.svg';
-import './App.css';
+import { useTranslation } from "react-i18next";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   const { t, i18n } = useTranslation();
 
   const changeLanguageHandler = (e) => {
-    const languageValue = e.target.value
+    const languageValue = e.target.value;
     i18n.changeLanguage(languageValue);
-  }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <select className="custom-select" style={{width: 200}} onChange={changeLanguageHandler}>
-          <option value="en" >English</option>
-          <option value="fr" >Français</option>
+        <select
+          className="custom-select"
+          style={{ width: 200 }}
+          onChange={changeLanguageHandler}
+        >
+          <option value="en">English</option>
+          <option value="fr">Français</option>
         </select>
-        <p>
-          {t('main.title')}
-        </p>
+        <p>{t("main.title")}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn translations
         </a>
       </header>
     </div>
