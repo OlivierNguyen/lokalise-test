062e37b20f8d7199153db6b263ef871982438490
Olivier NGUYEN <olivier_nguyen@live.fr>
2022-06-01 11:14:53 +0200

    feat: implement i18n files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { useTranslation } from 'react-i18next';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguageHandler = (e) => {
+    const languageValue = e.target.value
+    i18n.changeLanguage(languageValue);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <select className="custom-select" style={{width: 200}} onChange={changeLanguageHandler}>
+          <option value="en" >English</option>
+          <option value="fr" >Français</option>
+        </select>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {t('main.title')}
         </p>
         <a
           className="App-link"
