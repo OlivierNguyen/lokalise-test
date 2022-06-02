c54ab0d0c4377610a0cf4414babfb0249b99e879
Constant Druon <druon.constant@gmail.com>
2022-06-02 15:58:06 +0200

    test

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn about Lokalise
+          Learn about Lokalise!
         </a>
       </header>
     </div>
